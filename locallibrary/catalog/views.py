659f45cfce110d7bdd3c10f6c0b3f2ea30ba86c5
trangnh <hongtrangqn834@gmail.com>
2025-10-21 07:26:49 +0700

    Django Tutorial Part 6: Generic list and detail views

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,26 +1,31 @@
 from django.shortcuts import render
-
-# Create your views here.
-from catalog.models import Book, Author, BookInstance, Genre
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
-    """View function for home page of site."""
-
-    # Generate counts of some of the main objects
+    # Đây là view cho trang chủ, bạn có thể giữ nguyên hoặc tùy chỉnh
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-
-    # ... (Các dòng code khác sẽ được thêm vào sau)
-    # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-
-    # The 'all()' is implied by default.
     num_authors = Author.objects.count()
+
     context = {
-    'num_books': num_books,
-    'num_instances': num_instances,
-    'num_instances_available': num_instances_available,
-    'num_authors': num_authors,
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
     }
-    # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    
+    # Ghi đè phương thức để thêm dữ liệu tùy chỉnh vào template
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
